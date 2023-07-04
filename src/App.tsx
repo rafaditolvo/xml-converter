2f9cce54ebecd73e11698c77b8b58f5c75cb6445
Rafael Ditolvo <106878175+rafaditolvo@users.noreply.github.com>
2023-07-04 18:12:23 -0300

    front-updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
-import { Button } from "@chakra-ui/react";
+
 import Home from "./components/Home";
 
 const App = () => {
 
-
-  
   return (
     <div>
       <Home/>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, Input, Box, Heading } from '@chakra-ui/react';
+import { Button, Input, Box, Heading, Container, Stack, Image, Center } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
+import fileimg from "../images/file-backups-with-sequential-numbering.svg"
 
 const FileUpload: React.FC = () => {
   const [file, setFile] = React.useState<File | null>(null);
@@ -14,8 +15,7 @@ const FileUpload: React.FC = () => {
     if (file) {
       const formData = new FormData();
       formData.append('file', file);
-
-
+          
       fetch('/api/upload', {
         method: 'POST',
         body: formData,
@@ -32,31 +32,53 @@ const FileUpload: React.FC = () => {
   };
 
   return (
-    <Box p={4} boxShadow="md" rounded="md">
-      <Input type="file" onChange={handleFileChange} />
-      <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+    <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+      <Box p={8} boxShadow="md" rounded="md">
+        <Input type="file" onChange={handleFileChange} />
         <Button mt={4} colorScheme="teal" onClick={handleUpload}>
           Upload
         </Button>
-      </motion.div>
-    </Box>
+      </Box>
+    </motion.div>
   );
 };
 
 const Home: React.FC = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -20 }}
-    >
-      <Box p={4}>
-        <Heading as="h1" size="xl" mb={4}>
-          Conversor XML para Excel
-        </Heading>
-        <FileUpload />
-      </Box>
-    </motion.div>
+    <Box p={4}>
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}
+          >
+            Conversor XML para Excel
+          </Heading>
+        
+  <Center> <Image boxSize={"sm"} src={fileimg} ></Image></Center>
+ 
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}
+          >
+            <FileUpload />
+           
+            <Box>
+             
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
   );
 };
 
